475c278610b70f0851844bb21d491f7f9dac8356
Esteban Helguero <esteban.helguero@redk.net>
2021-04-01 23:41:58 +0200

    Added navbar with links to the pages and footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,16 @@
-function NavBar() {
+import Link from 'next/link'
+
+const Navbar = () => {
     return (
         <nav>
             <div className="logo">
                 <h1>Ninja List</h1>
             </div>
-            <a>Home</a>
-            <a>About</a>
-            <a>Ninja List</a>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/ninjas"><a>Ninja List</a></Link>
         </nav>
     )
 }
 
-export default NavBar
+export default Navbar
