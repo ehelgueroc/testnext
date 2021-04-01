b34678341905276196ecfaffca2c914eb8357152
Esteban Helguero <esteban.helguero@redk.net>
2021-04-01 12:11:01 +0200

    First commit

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>Ninja List</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>Ninja List</a>
+        </nav>
+    )
+}
+
+export default NavBar
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+function about() {
+    return (
+        <div>
+            <h1>About page</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos sequi sunt temporibus repellendus vel! Quae eveniet pariatur error totam alias. Architecto quam possimus exercitationem? Maiores nisi deleniti ipsam aspernatur. Doloremque.</p>
+        </div>
+    )
+}
+
+export default about
